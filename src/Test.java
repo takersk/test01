864447e58d48787659b29ac95922e55761f21483
takersk <takersk80@gmail.com>
2021-09-25 20:40:39 +0900

    test03 (#5)

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.printf("test01");
         System.out.printf("test02");
+        System.out.printf("test03");
     }
 }
